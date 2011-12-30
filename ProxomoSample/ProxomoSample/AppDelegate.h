3f6232dbbf2a4c6d1d3458cab36e81c85d65257b
Fred Crable <fred@additionsoftware.com>
2011-12-30 09:48:13 -0600

    Added start of sample app plus login handler

---
+++ ProxomoSample/ProxomoSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProxomoSample
+//
+//  Created by Fred Crable on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProxomoSample/ProxomoSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProxomoSample
+//
+//  Created by Fred Crable on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
